111363d8f8db72920ec53d769f995f3913c13eea
Anders Pitman <tapitman11@gmail.com>
2019-02-20 10:21:04 -0700

    Draft implementation of WriterAdapter working

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,6 +1,7 @@
-use omnistreams::{Consumer, WriteAdapter};
+use omnistreams::{Consumer, ConsumerEvent, WriteAdapter};
 use tokio::io;
 use tokio::prelude::*;
+use futures::future::lazy;
 
 
 trait Producer {
@@ -19,50 +20,80 @@ impl DummyProducer {
 impl Producer for DummyProducer {
 }
 
-struct Pipe<T> 
-    where T: Consumer + Future<Item=(), Error=io::Error> + Send + 'static,
-{
-    consumer_future: T,
-}
-
-impl<T> Pipe<T>
-    where T: Consumer + Future<Item=(), Error=io::Error> + Send + 'static,
-{
-    fn new(producer: impl Producer, consumer_future: T) -> Pipe<T> {
-
-        let consumer_message_tx = consumer_future.message_tx();
-
-        Pipe {
-            consumer_future,
-        }
-    }
-}
-
-impl<T> Future for Pipe<T>
-    where T: Consumer + Future<Item=(), Error=io::Error> + Send + 'static,
-{
-    type Item = ();
-    type Error = i32;
-
-    fn poll(&mut self) -> Poll<(), i32> {
-        Ok(Async::Ready(()))
-        //Ok(Async::NotReady)
-    }
-}
+//struct Pipe<T> 
+//    where T: Consumer + Future<Item=(), Error=io::Error> + Send + 'static,
+//{
+//    consumer_future: T,
+//}
+//
+//impl<T> Pipe<T>
+//    where T: Consumer + Future<Item=(), Error=io::Error> + Send + 'static,
+//{
+//    fn new(producer: impl Producer, consumer_future: T) -> Pipe<T> {
+//
+//        let consumer_message_tx = consumer_future.message_tx();
+//
+//        Pipe {
+//            consumer_future,
+//        }
+//    }
+//}
+//
+//impl<T> Future for Pipe<T>
+//    where T: Consumer + Future<Item=(), Error=io::Error> + Send + 'static,
+//{
+//    type Item = ();
+//    type Error = i32;
+//
+//    fn poll(&mut self) -> Poll<(), i32> {
+//        Ok(Async::Ready(()))
+//        //Ok(Async::NotReady)
+//    }
+//}
 
 
 fn main() {
 
-    let file_writer = tokio::fs::File::create("foo.txt");
-    let consumer = WriteAdapter::new(file_writer);
-    println!("{:?}", consumer);
-    //let consumer = consumer.map_err(|_| {});
+    //let file_writer = tokio::fs::File::create("foo.txt");
+    //let consumer = WriteAdapter::new(file_writer);
+    //println!("{:?}", consumer);
+    ////let consumer = consumer.map_err(|_| {});
 
-    let producer = DummyProducer::new();
+    //let producer = DummyProducer::new();
 
-    let pipe = Pipe::new(producer, consumer).map_err(|e| {
-        eprintln!("{:?}", e);
-    });
+    ////let pipe = Pipe::new(producer, consumer).map_err(|e| {
+    ////    eprintln!("{:?}", e);
+    ////});
 
-    tokio::run(pipe);
+    ////tokio::run(pipe);
+
+    tokio::run(lazy(|| {
+
+        let file_writer = tokio::fs::File::create("foo.txt");
+        let (mut consumer, event_stream) = WriteAdapter::new(file_writer);
+
+        let mut count = 0;
+
+        let data = vec![
+            vec![65, 66, 67],
+            vec![68, 69, 70],
+            vec![71, 72, 73],
+        ];
+
+        tokio::spawn(event_stream.for_each(move |event| {
+            if count < data.len() {
+                consumer.write(data[count].clone());
+                count += 1;
+            }
+            else {
+                consumer.end();
+            }
+            Ok(())
+        })
+        .map_err(|e| {
+            println!("error {:?}", e);
+        }));
+
+        Ok(())
+    }));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,60 +4,73 @@ use tokio::prelude::*;
 use futures::sync::mpsc;
 
 
-type MessageRx = mpsc::UnboundedReceiver<ConsumerMessage>;
-type MessageTx = mpsc::UnboundedSender<ConsumerMessage>;
+type MessageRx = mpsc::UnboundedReceiver<ConsumerMessage<Vec<u8>>>;
+type MessageTx = mpsc::UnboundedSender<ConsumerMessage<Vec<u8>>>;
+
+type EventRx = mpsc::UnboundedReceiver<ConsumerEvent>;
+type EventTx = mpsc::UnboundedSender<ConsumerEvent>;
+
 
 pub trait Consumer {
-    fn message_tx(&self) -> MessageTx;
+    fn write(&mut self, data: Vec<u8>);
+    fn end(&mut self);
+    fn next_event(&mut self) -> Option<ConsumerEvent>;
+    //fn event_stream(&mut self) -> Stream<Item=ConsumerEvent, Error=io::Error>;
 }
 
 #[derive(Debug)]
-pub enum ConsumerMessage {
-    Write(Vec<u8>),
+pub enum ConsumerMessage<T> {
+    Write(T),
     End,
 }
 
+#[derive(Debug)]
 pub enum ConsumerEvent {
+    Request(usize),
 }
 
 #[derive(Debug)]
-pub struct WriteAdapter<T, U>
-    where T: Future<Item=U, Error=io::Error>,
-          U: AsyncWrite,
+pub struct WriteAdapter
 {
-    writer_future: T,
-    writer: Option<U>,
-    mrx: MessageRx,
-    message_tx: MessageTx,
+    message_tx: mpsc::UnboundedSender<ConsumerMessage<Vec<u8>>>,
+    //event_rx: EventRx,
 }
 
-impl<T, U> WriteAdapter<T, U>
+#[derive(Debug)]
+enum WriteAdapterState<T, U>
     where T: Future<Item=U, Error=io::Error>,
           U: AsyncWrite,
 {
-    pub fn new(writer_future: T) -> WriteAdapter<T, U> {
-        
-        let (message_tx, mrx) = mpsc::unbounded::<ConsumerMessage>();
+    WaitingForWriter(T),
+    Writing(U),
+}
 
-        WriteAdapter {
-            writer_future,
-            writer: None,
-            mrx,
-            message_tx,
+struct InnerTask<T, U>
+    where T: Future<Item=U, Error=io::Error>,
+          U: AsyncWrite,
+{
+    state: WriteAdapterState<T, U>,
+    message_rx: MessageRx,
+    event_tx: EventTx,
+}
+
+impl<T, U> InnerTask<T, U>
+    where T: Future<Item=U, Error=io::Error>,
+          U: AsyncWrite,
+{
+    fn new(writer_future: T, message_rx: MessageRx, event_tx: EventTx) -> InnerTask<T, U> {
+
+        (&event_tx).send(ConsumerEvent::Request(1));
+
+        InnerTask {
+            state: WriteAdapterState::WaitingForWriter(writer_future),
+            message_rx,
+            event_tx,
         }
     }
 }
 
-impl<T, U> Consumer for WriteAdapter<T, U>
-    where T: Future<Item=U, Error=io::Error>,
-          U: AsyncWrite,
-{
-    fn message_tx(&self) -> MessageTx {
-        self.message_tx.clone()
-    }
-}
-
-impl<T, U> Future for WriteAdapter<T, U>
+impl<T, U> Future for InnerTask<T, U>
     where T: Future<Item=U, Error=io::Error>,
           U: AsyncWrite,
 {
@@ -66,56 +79,147 @@ impl<T, U> Future for WriteAdapter<T, U>
 
     fn poll(&mut self) -> Poll<(), io::Error> {
 
-        match self.mrx.poll().unwrap() {
-            Async::Ready(Some(ConsumerMessage::Write(data))) => {
-                println!("write");
-                Ok(Async::NotReady)
-            },
-            Async::Ready(Some(ConsumerMessage::End)) => {
-                println!("end");
-                Ok(Async::NotReady)
-            },
-            Async::NotReady => {
-                Ok(Async::NotReady)
-            },
-            _ => {
-                Ok(Async::NotReady)
+        // process any messages from upstream
+        loop {
+            match self.message_rx.poll().unwrap() {
+                Async::Ready(Some(ConsumerMessage::Write(data))) => {
+                    println!("write {:?}", data);
+                    let tx = &self.event_tx;
+                    tx.send(ConsumerEvent::Request(1));
+                    task::current().notify();
+                },
+                Async::Ready(Some(ConsumerMessage::End)) => {
+                    println!("end");
+                    return Ok(Async::Ready(()));
+                },
+                Async::NotReady => {
+                    break;
+                },
+                _ => {
+                    break;
+                }
             }
         }
 
-        //if self.writer.is_none() {
-        //    match self.writer_future.poll() {
-        //        Ok(Async::Ready(writer)) => {
-        //            self.writer = Some(writer);
-
-        //            task::current().notify();
-        //            Ok(Async::NotReady)
-        //        },
-        //        Ok(Async::NotReady) => {
-        //            Ok(Async::NotReady)
-        //        },
-        //        Err(e) => {
-        //            Err(e)
-        //        },
-        //    }
-        //}
-        //else {
-        //    let writer = self.writer.as_mut().unwrap();
-        //    match writer.poll_write(b"Hi there") {
-        //        Ok(Async::Ready(n)) => {
-        //          println!("wrote: {:?}", n);
-        //          Ok(Async::Ready(()))
-        //        },
-        //        Ok(Async::NotReady) => {
-        //          Ok(Async::NotReady)
-        //        },
-        //        Err(e) => {
-        //            Err(e)
-        //        }
-        //    }
-        //}
+        Ok(Async::NotReady)
     }
 }
+
+impl WriteAdapter
+{
+    pub fn new<T, U>(writer_future: T) -> (WriteAdapter, EventRx)
+        where T: Future<Item=U, Error=io::Error> + Send + 'static,
+              U: AsyncWrite + Send + 'static,
+    {
+        let (message_tx, message_rx) = mpsc::unbounded::<ConsumerMessage<Vec<u8>>>();
+        let (event_tx, event_rx) = mpsc::unbounded::<ConsumerEvent>();
+
+        let inner_task = InnerTask::new(writer_future, message_rx, event_tx);
+        tokio::spawn(inner_task.map_err(|_| {}));
+
+        (WriteAdapter {
+            message_tx,
+            //event_rx,
+        }, event_rx)
+    }
+
+    //pub fn event_stream(&mut self) -> impl Stream<Item=ConsumerEvent, Error=io::Error>
+    //{
+    //    //futures::stream::ok(ConsumerEvent::Request(1))
+    //    self.event_rx
+    //}
+
+}
+
+impl Consumer for WriteAdapter {
+    fn write(&mut self, data: Vec<u8>) {
+        let tx = &self.message_tx;
+        tx.send(ConsumerMessage::Write(data));
+    }
+
+    fn end(&mut self) {
+        let tx = &self.message_tx;
+        tx.send(ConsumerMessage::End);
+    }
+
+    fn next_event(&mut self) -> Option<ConsumerEvent> {
+        //match self.event_rx.poll().unwrap() {
+        //    Async::Ready(Some(event)) => {
+        //        Some(event)
+        //    },
+        //    Async::NotReady => {
+        //        None
+        //    },
+        //    _ => {
+        //        None
+        //    }
+        //}
+        None
+    }
+
+}
+
+//impl<T, U> Future for WriteAdapter<T, U>
+//    where T: Future<Item=U, Error=io::Error>,
+//          U: AsyncWrite,
+//{
+//    type Item = ();
+//    type Error = io::Error;
+//
+//    fn poll(&mut self) -> Poll<(), io::Error> {
+//
+//        Ok(Async::Ready(()))
+//
+//        //match self.mrx.poll().unwrap() {
+//        //    Async::Ready(Some(ConsumerMessage::Write(data))) => {
+//        //        println!("write");
+//        //        Ok(Async::NotReady)
+//        //    },
+//        //    Async::Ready(Some(ConsumerMessage::End)) => {
+//        //        println!("end");
+//        //        Ok(Async::NotReady)
+//        //    },
+//        //    Async::NotReady => {
+//        //        Ok(Async::NotReady)
+//        //    },
+//        //    _ => {
+//        //        Ok(Async::NotReady)
+//        //    }
+//        //}
+//
+//        //if self.writer.is_none() {
+//        //    match self.writer_future.poll() {
+//        //        Ok(Async::Ready(writer)) => {
+//        //            self.writer = Some(writer);
+//
+//        //            task::current().notify();
+//        //            Ok(Async::NotReady)
+//        //        },
+//        //        Ok(Async::NotReady) => {
+//        //            Ok(Async::NotReady)
+//        //        },
+//        //        Err(e) => {
+//        //            Err(e)
+//        //        },
+//        //    }
+//        //}
+//        //else {
+//        //    let writer = self.writer.as_mut().unwrap();
+//        //    match writer.poll_write(b"Hi there") {
+//        //        Ok(Async::Ready(n)) => {
+//        //          println!("wrote: {:?}", n);
+//        //          Ok(Async::Ready(()))
+//        //        },
+//        //        Ok(Async::NotReady) => {
+//        //          Ok(Async::NotReady)
+//        //        },
+//        //        Err(e) => {
+//        //            Err(e)
+//        //        }
+//        //    }
+//        //}
+//    }
+//}
 
 #[cfg(test)]
 mod tests {
